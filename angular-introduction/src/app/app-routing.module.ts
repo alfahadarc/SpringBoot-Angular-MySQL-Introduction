3cfdf05282b8f49d9b021cd31041a6e04b333ba0
alfahadarc <alfahadarc@gmail.com>
2023-05-29 21:14:28 +0600

    bookui added

--- angular-introduction/src/app/app-routing.module.ts
+++ angular-introduction/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { BooksuiComponent } from './components/booksui/booksui.component';
 import { ShopuiComponent } from './components/shopui/shopui.component';
+import { AddbookComponent } from './components/booksui/addbook/addbook.component';
 
 const routes: Routes = [
   { path: 'book', component: BooksuiComponent },
-  { path: 'shop', component: ShopuiComponent }
+  { path: 'shop', component: ShopuiComponent },
+  {path:'addBook', component:AddbookComponent}
 ];
 
 @NgModule({
--- angular-introduction/src/app/app.module.ts
+++ angular-introduction/src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { BooksuiComponent } from './components/booksui/booksui.component';
 import { ShopuiComponent } from './components/shopui/shopui.component';
+import { AddbookComponent } from './components/booksui/addbook/addbook.component';
+import { ListbookComponent } from './components/booksui/listbook/listbook.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     BooksuiComponent,
-    ShopuiComponent
+    ShopuiComponent,
+    AddbookComponent,
+    ListbookComponent,
   ],
   imports: [
     BrowserModule,
