b97d6a9e3c24be20d5a50b50d176e990b8644c57
alfahadarc <alfahadarc@gmail.com>
2023-05-29 21:29:43 +0600

    material added

--- angular-introduction/src/app/app.module.ts
+++ angular-introduction/src/app/app.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+//Materials
+import { MaterialModule } from 'src/app/material/material.module';
+
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { BooksuiComponent } from './components/booksui/booksui.component';
 import { ShopuiComponent } from './components/shopui/shopui.component';
 import { AddbookComponent } from './components/booksui/addbook/addbook.component';
 import { ListbookComponent } from './components/booksui/listbook/listbook.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -17,8 +21,10 @@ import { ListbookComponent } from './components/booksui/listbook/listbook.compon
     ListbookComponent,
   ],
   imports: [
+    MaterialModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ angular-introduction/src/app/material/material.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+const MaterialComponents = [
+    MatButtonModule,
+  ];
+
+@NgModule({
+    exports: [MaterialComponents],
+    imports: [MaterialComponents],
+  })
+  export class MaterialModule {}
